f554501ebd5c2bbb2b631821fe90ae1f025a0c00
Sava <Sava.meteor26@gmail.com>
2023-10-10 13:37:19 +0300

    Task

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int ticket = 12600;
+        int bonus = (20 * 1);
+        int milies = (ticket / bonus);
+
+        System.out.println("Будем начислено" + " " + milies +  " " + "милей за билет стоимостью" + " " + ticket );
+
+    }
+}
